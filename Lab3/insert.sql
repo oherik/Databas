82fd8fb6acb17af631fe3ce432e8912feecf688d
Erik Öhrn <erik.ohrn@outlook.com>
2016-02-11 13:54:53 +0100

    Added some tests and stuff to the inserts

--- Lab3/insert.sql
+++ Lab3/insert.sql
@@ -1,7 +1,34 @@
 INSERT INTO Department
-VALUES('Informationsteknik', 'IT'),
-VALUES('Datateknik', 'D'),
-VALUES('Elektroteknik', 'E');
+--	Should throw errors:
+-- VALUES('Test', 'TOOMANYLETTERS'),
+-- VALUES('', 'QQ'),
+-- VALUES('Test', ''),
+--	Good values:
+VALUES
+	('Computer Science and Engineering', 'CSE'),
+	('Mechanical Engineering', 'ME');
+
+INSERT INTO Programme
+--	Should throw errors:
+-- VALUES('Test', 'TOOMANYLETTERS'),
+-- VALUES('', 'QQ'),
+-- VALUES('Test', ''),
+--	Good values:
+VALUES
+	('Applied Information Technology', 'IT'),
+	('Computer Science', 'D'),
+	('Electronic Engineering', 'E'),
+	('Technical Design', 'TD'),
+	('Mechanical Engineering', 'M');
 
 INSERT INTO Course
-VALUES('TDA367', 7.5, 'Databaser', 'IT');
+--	Should throw errors:
+-- VALUES('Test', 1, 'Test', 'WWW'),
+-- VALUES('Test', 1, 'Test', 'TOOMANYLETTERS'),
+-- VALUES('Test', '', 'Test', 'CSE'),
+-- VALUES('Test', 'One', 'Test', 'CSE'),
+-- VALUES('', 1, 'Test', '
+-- VALUES('Test', 1, 'Test', ''),
+-- VALUES('Test', 1, '', 'CSE'),
+--	Good values:
+VALUES('TDA367', 7.5, 'Databaser', 'CSE');
