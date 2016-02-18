c7713a602dd7239b954ffbc9c21979d93f22d559
LovreB <inabergbom@gmail.com>
2016-02-18 16:02:16 +0100

    corrected StudentsFollowing, started with PathToGraduation

--- Lab3/03views.sql
+++ Lab3/03views.sql
@@ -1,37 +1,41 @@
 CREATE VIEW StudentsFollowing AS
 SELECT  NationalID,
-SchoolID,
-Name,
-Student.Programme,
-Branch
-FROM    Student JOIN StudiesBranch
+        SchoolID,
+        Name,
+        Student.Programme,
+        Branch
+FROM    Student FULL OUTER JOIN StudiesBranch
 ON      StudiesBranch.Student = Student.NationalID;
 
 CREATE VIEW FinishedCourses AS
 SELECT  Student,
-Course.Name AS CourseName,
-Grade,
-Credit
+        Course.Code AS CourseCode,
+        Course.Name AS CourseName,
+        Grade,
+        Credit
 FROM    HasFinished JOIN Course
 ON      HasFinished.Course = Course.Code;
 
 CREATE VIEW Registrations AS
 SELECT  Student,
-Code AS CourseCode,
-'Waiting' AS Status
+        Code AS CourseCode,
+        Name AS CourseName,
+        'Waiting' AS Status
 FROM    IsOnWaitingList JOIN Course
 ON      IsOnWaitingList.RestrictedCourse = Course.Code
 UNION
 SELECT  Student,
-Code AS CourseCode,
-'Registred' AS Status
+        Code AS CourseCode,
+        Name AS CourseName,
+        'Registred' AS Status
 FROM    RegisteredOn JOIN Course
 ON      RegisteredOn.Course = Course.Code;
 
 CREATE VIEW PassedCourses AS
 SELECT  Student,
-Code AS CourseCode,
-Credit
+        Code AS CourseCode,
+        Name AS CourseName,
+        Credit
 FROM    HasFinished JOIN Course
 ON      HasFinished.Course = Course.Code
 WHERE   Grade <> 'U';
@@ -43,7 +47,7 @@ FROM
 	StudentsFollowing JOIN ProgrammeHasMandatory
  	ON StudentsFollowing.Programme = ProgrammeHasMandatory.Programme
 UNION DISTINCT
-SELECT NationalID, 
+SELECT NationalID,
 		Course
 FROM
 	StudentsFollowing JOIN BranchHasMandatory
@@ -53,4 +57,35 @@ EXCEPT
 SELECT Student, CourseCode
 FROM PassedCourses
 ;
+CREATE VIEW PathToGraduation AS
+SELECT NationalID AS StudentID,
+        Sum(Credit) AS TotalCredit
+        --Count(Unread_Course) AS NbrMandatoryLeft
+        --MathCredits,
+        --ResearchCredit,
+        --NbrOfSeminarCourses,
+        --Qualify
+FROM StudentsFollowing JOIN PassedCourses
+ON      StudentsFollowing.NationalID = PassedCourses.Student
+     --   JOIN UnreadMandatory
+--ON      StudentsFollowing.NationalID = UnreadMandatory.Student
+GROUP BY StudentID
+UNION DISTINCT
+-- When no courses are passed
+SELECT NationalID AS StudentID,
+        0 AS TotalCredit
+        --Count(Unread_Course) AS NbrMandatoryLeft
+        --MathCredits,
+        --ResearchCredit,
+        --NbrOfSeminarCourses,
+        --Qualify
+FROM StudentsFollowing JOIN PassedCourses
+ON      StudentsFollowing.NationalID <> PassedCourses.Student
+       -- JOIN UnreadMandatory
+--ON      StudentsFollowing.NationalID = UnreadMandatory.Student
+GROUP BY StudentID
+;
 
+
+
+--TODO ändra till SchoolID
