aff4dea37977dfd6926d80aa05be42fc687c087d
LovreB <inabergbom@gmail.com>
2016-02-18 21:22:37 +0100

    MathCourses and ResearchCourses added to PathToGraduation

--- Lab3/03views.sql
+++ Lab3/03views.sql
@@ -56,30 +56,53 @@ FROM
 EXCEPT
 SELECT Student, CourseCode
 FROM PassedCourses
-ORDER BY Student DESC ;
+ORDER BY Student DESC
 ;
 CREATE VIEW PathToGraduation AS
-        SELECT
-        StudentID,
-        TotalCredit,
-        Count(Unread_Course) AS NbrMandatoryLeft
-        --MathCredits,
-        --ResearchCredit,
-        --NbrOfSeminarCourses,
-        --Qualify
+SELECT
+    TotalCreditTable.StudentID,
+    TotalCredit,
+    NbrMandatoryLeft,
+    MathCredit,
+    ResearchCredit
+    --NbrOfSeminarCourses,
+    --Qualify
 FROM (
-        SELECT NationalID AS StudentID,
-                Sum(Credit) AS TotalCredit
-        FROM StudentsFollowing
-                FULL OUTER JOIN PassedCourses
-        ON      StudentsFollowing.NationalID = PassedCourses.Student
-        GROUP BY StudentID) AS SumCredit
-FULL OUTER JOIN UnreadMandatory
-ON      SumCredit.StudentID = UnreadMandatory.Student
-GROUP BY StudentID,TotalCredit
-ORDER BY StudentID DESC ;
+    SELECT NationalID AS StudentID,
+            Sum(Credit) AS TotalCredit
+    FROM    PassedCourses
+            FULL OUTER JOIN StudentsFollowing
+    ON      StudentsFollowing.NationalID = PassedCourses.Student
+    GROUP BY StudentID) AS TotalCreditTable
+  FULL OUTER JOIN (
+    SELECT Student AS StudentID,
+            sum(Credit) AS MathCredit
+    FROM    PassedCourses
+    FULL OUTER JOIN HasClassification
+    ON      HasClassification.Course = PassedCourses.CourseCode
+    WHERE HasClassification.Classification = 'Mathematical'
+    GROUP BY StudentID) AS MathCreditTable
+  ON (TotalCreditTable.StudentID = MathCreditTable.StudentID)
+  FULL OUTER JOIN (
+    SELECT Student AS StudentID,
+            sum(Credit) AS ResearchCredit
+    FROM    PassedCourses
+    LEFT OUTER JOIN HasClassification
+    ON      HasClassification.Course = PassedCourses.CourseCode
+    WHERE HasClassification.Classification = 'Research'
+    GROUP BY StudentID) AS ResearchCreditTable
+  ON (TotalCreditTable.StudentID = ResearchCreditTable.StudentID)
+  FULL OUTER JOIN(
+    SELECT UnreadMandatory.Student AS StudentID,
+            Count(Unread_Course) AS NbrMandatoryLeft
+    FROM    UnreadMandatory
+    GROUP BY StudentID) AS UnreadMandatoryTable
+  ON (TotalCreditTable.StudentID = UnreadMandatoryTable.StudentID)
+GROUP BY TotalCreditTable.StudentID,
+    TotalCredit,
+    NbrMandatoryLeft,
+    MathCredit,
+    ResearchCredit
 ;
-
-
 
 --TODO ändra till SchoolID
