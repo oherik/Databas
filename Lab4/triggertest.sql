2de59ef64af140f8e3a24f77f1b819a0b162bccb
Erik Öhrn <erik.ohrn@outlook.com>
2016-02-29 11:58:52 +0100

    Added outputs for the tests, as well as test 1-4

--- Lab4/triggertest.sql
+++ Lab4/triggertest.sql
@@ -1,5 +1,3 @@
-
-
 -- TESTS FOR TRIGGERS
 -- Should not throw error, existing student and a restricted course with another person waiting. Should be put on waiting List
 INSERT INTO Registrations VALUES('851007-9091','MED21');
@@ -15,6 +13,26 @@ INSERT INTO Registrations VALUES('851007-9091', 'TDA383');
 
 -- TESTCASES
 
--- 4. Should not be registered after these calls.
+-- 1. 
+\echo '------------------------------------------ \n Test 1: List info for a student \n'
+SELECT * FROM PathToGraduation WHERE Student = '620314-2044';
+
+-- 2. 
+\echo '------------------------------------------ \n Test 2: Register the student for an unrestricted course, \n and show that they end up registered (show info again).\n'
+SELECT * FROM Registrations WHERE CourseCode = 'POL227';
+\echo '\nAdding 620314-2044 to POL227\n'
+INSERT INTO Registrations VALUES ('620314-2044', 'POL227');
+SELECT * FROM Registrations WHERE CourseCode = 'POL227';
+
+-- 3. 
+\echo '------------------------------------------ \n Test 3: Register the same student for the same course again, \n and show that the program doesn’t crash, and that the student gets an error message.\n'
+INSERT INTO Registrations VALUES ('620314-2044', 'POL227');
+
+-- 4. 
+\echo '------------------------------------------ \n Test 4: Should not be registered after these calls.\n'
 DELETE FROM Registrations WHERE Student = '19650430-7734' AND CourseCode = 'TDA358';
 DELETE FROM Registrations WHERE Student = '19650430-7734' AND CourseCode = 'TDA358';
+
+-- 5.
+\echo '------------------------------------------ \n Test 5: Register the student for a course that they don’t have \n the prerequisites for, and show that the registration doesn’t go through.\n'
+	 
