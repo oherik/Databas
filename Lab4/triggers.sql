0fc9dd98605e0dae4d7d60de9d9cbc9a019a9796
LovreB <inabergbom@gmail.com>
2016-03-07 16:11:25 +0100

    Fixec compiling errors in trigger

--- Lab4/triggers.sql
+++ Lab4/triggers.sql
@@ -23,20 +23,20 @@ DECLARE isRegistered BOOLEAN;
 DECLARE hasPassed BOOLEAN;
 DECLARE hasReadPrerequisites BOOLEAN;
 DECLARE maxStudents  INT;
-DECLARE registeredStudents INT;
+DECLARE registredStudents INT;
 DECLARE nbrSpotsLeft INT;
 
 	BEGIN
-		maxStudents := (SELECT RestrictedCourse.MaxStudents FROM RestrictedCourse 
-			WHERE (Code = OLD.CourseCode));
-      	registredStudents := (SELECT count(Student) FROM Registrations 
-      		WHERE Status = 'Registred' AND Registrations.CourseCode = OLD.CourseCode);
+		maxStudents := (SELECT RestrictedCourse.MaxStudents FROM RestrictedCourse
+			WHERE (Code = NEW.CourseCode));
+      	registredStudents := (SELECT count(Student) FROM Registrations
+      		WHERE Status = 'Registred' AND Registrations.CourseCode = NEW.CourseCode);
       	nbrSpotsLeft := (SELECT maxStudents-registredStudents);
-		queueLength := (SELECT MAX(QueuePos) FROM IsOnWaitingList 
-			WHERE NEW.CourseCode = IsOnWaitingList.RestrictedCourse);	
-		
+		queueLength := (SELECT MAX(QueuePos) FROM IsOnWaitingList
+			WHERE NEW.CourseCode = IsOnWaitingList.RestrictedCourse);
+
 		/*
-		 Check if the student can be added to the course or waiting list. 
+		 Check if the student can be added to the course or waiting list.
 		 */
 		isWaiting := (SELECT EXISTS(SELECT 1 FROM IsOnWaitingList WHERE
 				NEW.Student = IsOnWaitingList.Student AND NEW.CourseCode = IsOnWaitingList.RestrictedCourse));
@@ -49,7 +49,7 @@ DECLARE nbrSpotsLeft INT;
 		EXCEPT
 		SELECT Course FROM HasFinished WHERE HasFinished.Student = NEW.Student) as CoursesLeft
 		WHERE CoursesLeft.Course IS NOT NULL LIMIT 1), true));
-		
+
 		IF isRegistered THEN
 			RAISE NOTICE 'The student % is already registered on the course %.', NEW.Student, NEW.CourseCode;
 		ELSEIF isWaiting THEN
@@ -62,10 +62,10 @@ DECLARE nbrSpotsLeft INT;
 		ELSE
 		-- Add the student to the appropriate table
 			IF queueLength > 0 OR nbrSpotsLeft < 1 THEN
-				INSERT INTO IsOnWaitingList(Student, RestrictedCourse, QueuePos) 
+				INSERT INTO IsOnWaitingList(Student, RestrictedCourse, QueuePos)
 					VALUES(NEW.Student, NEW.CourseCode, queueLength+1);
 			ELSE
-				INSERT INTO RegisteredOn(Student, Course) 
+				INSERT INTO RegisteredOn(Student, Course)
 					VALUES(NEW.Student, NEW.CourseCode);
 			END IF;
 		END IF;
@@ -74,22 +74,22 @@ DECLARE nbrSpotsLeft INT;
 $register$ LANGUAGE plpgsql;
 
 CREATE TRIGGER register INSTEAD OF INSERT ON Registrations
-	FOR EACH ROW EXECUTE PROCEDURE register();	
+	FOR EACH ROW EXECUTE PROCEDURE register();
 
 /*
-when a student tries to register for a course that is full, that student is added to the waiting list for the course. 
-Be sure to check that the student may actually register for the course before adding to either list, if it may not you 
-should raise an error (use RAISE EXCEPTION). Hint: There are several requirements for registration stated in the domain 
+when a student tries to register for a course that is full, that student is added to the waiting list for the course.
+Be sure to check that the student may actually register for the course before adding to either list, if it may not you
+should raise an error (use RAISE EXCEPTION). Hint: There are several requirements for registration stated in the domain
 description, and some implicit ones like that a student can not be both waiting and registered for the same course at the same time.
 
-when a student unregisters from a course if the student was properly registered and not only on the waiting list, the 
-first student (if any) in the waiting list should be registered for the course instead. Note: this should only be done 
-if there is actually room on the course (the course might have been over-full due to an administrator overriding the 
+when a student unregisters from a course if the student was properly registered and not only on the waiting list, the
+first student (if any) in the waiting list should be registered for the course instead. Note: this should only be done
+if there is actually room on the course (the course might have been over-full due to an administrator overriding the
 restriction and adding students directly).
 
-You need to write the triggers on the view Registrations instead of on the tables themselves (third bullet under task 3 above). 
-(One reason for this is that we “pretend” that you only have the privileges listed under Task 4, which means you cannot insert 
-data into, or delete data from, the underlying tables directly. But even if we lift this restriction, there is another reason 
+You need to write the triggers on the view Registrations instead of on the tables themselves (third bullet under task 3 above).
+(One reason for this is that we “pretend” that you only have the privileges listed under Task 4, which means you cannot insert
+data into, or delete data from, the underlying tables directly. But even if we lift this restriction, there is another reason
 for not defining these triggers on the underlying tables - can you figure out why?)
 */
 CREATE FUNCTION unregister_check() RETURNS TRIGGER AS $$
